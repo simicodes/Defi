ce967f687128652009fe9c71a1a96418d8ba76b6
simicodes <simeonoffiong@gmail.com>
2022-07-08 09:24:47 -0600

    Updated

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-black text-white">
+      <div className="max-w-[1240px] mx-auto px-4">
+        <div>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token List</li>
+            <li>Defi</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
